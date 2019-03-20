162890e93a43db9dcebbd97653243c96b0709b0b
Shubham Jain <s.shubjain@gmail.com>
2019-03-21 04:38:49 +0530

    added uuid package and minor adjustments

--- App.js
+++ App.js
@@ -1,33 +1,29 @@
 import React from 'react';
-
 import { View, StatusBar } from 'react-native';
-
 import { Provider } from 'react-redux';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import { Provider as PaperProvider } from 'react-native-paper';
+
 import NavigationService from 'utils/NavigationService';
 import { PersistGate } from 'redux-persist/integration/react';
 import { styles, Theme, papertheme } from 'theme/index';
+import { sentryConfig } from 'config/keys';
 
 import Sentry from 'sentry-expo';
-
-import { sentryConfig } from 'config/keys';
 
 import store, { persistor } from './redux/store';
 
 import AppNavigator from './navigation/AppNavigator';
 
 // Remove this once Sentry is correctly setup.
-Sentry.enableInExpoDevelopment = true;
-
-Sentry.config(sentryConfig).install();
+// Sentry.enableInExpoDevelopment = true;
+Sentry.config(sentryConfig.SENTRY_DSN).install();
 
 
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false
   };
-
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
